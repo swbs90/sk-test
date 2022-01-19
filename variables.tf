ee533237113574874f7b83b44d42b92f350fb328
s2leeee <97569825+s2leeee@users.noreply.github.com>
2022-01-19 14:23:07 +0900

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,124 @@
+#===========================#
+# VMware vCenter connection #
+#===========================#
+
+variable "vsphere-user" {
+type = string 
+description = "vCenter/vSphere 계정"
+}  
+
+variable "vsphere-password" {
+type = string
+description = "vCenter/vSphere 패스워드"
+}
+
+variable "vsphere-vcenter" {
+type = string
+description = "vCenter/vSphere 서버 FQDN / IP"
+}
+
+variable "vsphere-unverified-ssl" {
+type = string
+description = "자체 서명 된 인증서를 사용하는 VMware vCenter (true / false)"
+}
+
+variable "vsphere-datacenter" {
+type = string
+description = "VMWare vSphere datacenter 이름"
+}
+
+variable "vsphere-cluster" {
+type = string
+description = "VMWare vSphere cluster 이름"
+default = ""
+}
+
+variable "vsphere-template-folder" {
+type = string
+description = "Template folder 이름"
+default = "template-folder"
+}
+
+#================================#
+# VMware vSphere virtual machine #
+#================================#
+
+variable "vm-count" {
+type = string
+description = "생성 할 VM 갯수"
+default     =  1
+}
+
+variable "vm-datastore" {
+type = string
+description = "VMWare vSphere datastore 이름"
+}
+
+variable "vm-network" {
+type = string
+description = "VMWare vSphere Network 이름"
+}
+
+variable "vm-cpu" {
+type = string
+description = "가상머신 CPU 개수"
+default = "2"
+}
+
+variable "vm-ram" {
+type = string
+description = "가상머신 Memory 사이즈"
+default = "2048"
+}
+
+variable "vm-disk" {
+type = string
+description = "가상머신 Disk 사이즈"
+default = "30"
+}
+
+variable "vm-name" {
+type = string
+description = "가상머신 이름"
+}
+
+#variable "vm-firmware" {
+#type = string
+#description = "가상머신 fireware set (efi / bios)"
+#default = "efi"
+#}
+
+variable "vm-guest-id" {
+type = string
+description = "가상머신 OS명"
+}
+
+variable "vm-template-name" {
+type = string
+description = "가상머신을 배포할 템플릿 이름"
+}
+
+variable "vm-dvs"{
+type = string
+description = "분산 스위치"
+}
+
+variable "vm-pg"{
+type = string
+description = "분산 스위치 포트그룹"
+}
+
+variable "vm-ipv4"{
+type = string
+description = "ipv4 address"
+}
+
+variable "vm-netmask"{
+type = string
+description = "netmask"
+}
+
+variable "vm-gw4"{
+type = string
+description = "gateway"
+}
